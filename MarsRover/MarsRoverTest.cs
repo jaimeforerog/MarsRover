4a4baae2bf341613d090ddce61e00a9ab6d6067e
Jaime Forero <jaime.forero@sinco.com.co>
2025-10-18 18:59:02 -0500

    🟥🟥  Envio Vacio 1  prueba fallando

---
+++ MarsRover/MarsRoverTest.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+
+namespace MarsRover;
+
+public class MarsRoverTest
+{
+    [Fact]
+    public void SiEnvio_vacio_Debe_Retornar_00_N()
+    {
+        //arrenge
+        string movimiento = "";
+        //Act
+        string resultado = RealizarMovimiento( movimiento);
+        //Assert
+        resultado.Should().Be("0,0:N");
+
+    }
+
+    private string RealizarMovimiento(object numero)
+    {
+        throw new NotImplementedException();
+    }
+}
